e6d2a3065c64adebae1cdd926972ced23084fe41
Leonidas <jleones@vallenevado.com>
2021-05-17 22:12:37 -0400

    dashboard change name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">BackOffice</div>
+                <div class="card-header">atrasoficina</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    
                     You are logged in!
                 </div>
             </div>
